b9c7bd13c6567f88b9137813c623fe5cb0dd5c7d
user <user@BU6936.eame.global.sgs.com>
2018-08-27 14:21:52 +0300

    1-st commit 27.08.2018

---
+++ src/com/dpp/Main.java
@@ -0,0 +1,10 @@
+package com.dpp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        System.out.println(args[0]);
+    }
+}
